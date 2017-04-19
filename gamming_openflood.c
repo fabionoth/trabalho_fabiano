2f3b19eefd683f270112ce2259ccde6284789c34
Fabio Noth <fabio.noth@gmail.com>
2017-04-19 11:46:30 -0300

    inform integer value with interval

--- gamming_openflood.c
+++ gamming_openflood.c
@@ -19,53 +19,105 @@
 #define DEBUG 1
 #define width 10
 #define height 10
+#define num_colors 4
 
 int map[width][height];
+int count;
 
 void read_map(const char* file_name);
 void print_map();
+int read_integer();
+int verify_integer(char *numArray);
 
 main()
 {
-	char filename[8] = "map.txt";
-	read_map(filename); 
-	print_map();
+    count = 0;
+    char filename[8] = "map.txt";
+
+    read_map(filename); 
+    print_map();
+    read_integer();
 }
+
 
 void read_map (const char* file_name)
 {
-	FILE* file = fopen (file_name, "r");
-	int i, j, k;
-	i = 0;
-	j = 0;
-	k = 0;
+    FILE* file = fopen (file_name, "r");
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = 0;
 
-	fscanf (file, "%d", &i);    
-	while (!feof (file))
-	{ 
-		if(j == width)
-		{
-			j = 0;
-			k++;
-		}		
-		map[k][j] = i;
-		j++;
-		fscanf (file, "%d", &i);    
-	}
-	fclose (file);        
+    fscanf (file, "%d", &i);    
+    while (!feof (file))
+    { 
+        if(j == width)
+        {
+            j = 0;
+            k++;
+        }		
+        map[k][j] = i;
+        j++;
+        fscanf (file, "%d", &i);    
+    }
+    fclose (file);        
 }
 void print_map()
 {
-	int k, j;
-	for( k = 0; k < width; k++)
-	{
-		for( j = 0; j < height; j++)
-		{
-			printf("%i ", map[k][j]);
-		}
-		printf("\n");
-	}
+    int k, j;
+    for( k = 0; k < width; k++)
+    {
+        for( j = 0; j < height; j++)
+        {
+            printf("%i ", map[k][j]);
+        }
+        printf("\n");
+    }
 }
+
+int read_integer()
+{
+    int start = 1;
+    int ret = 0;
+    char read[255];
+    printf("Inform the value from %i to %i for map: ", start, num_colors);
+    scanf("%s", &read);
+    if(!verify_integer(read)){
+        ret = (int)strtol(read, (char **)NULL, 10);
+        if(ret < start || ret > num_colors){
+            read_integer();
+        }
+       
+    }
+    ret = (int)strtol(read, (char **)NULL, 10);
+    return ret;
+}
+
+int verify_integer(char *numArray)
+{
+    int i;
+    const int len = sizeof(numArray);
+
+    for (i = 0; i < len; i++)
+    {
+        if (!isdigit(numArray[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
